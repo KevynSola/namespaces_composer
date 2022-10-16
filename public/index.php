dacdebc8873a1d96899eacd92f89219927fb8073
Kevyn SOLA <kevyn.sola@gmail.com>
2022-10-16 16:44:18 +0200

    add cow package and modif index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Hello;
+use CowSay\Cow;
 
 require '../vendor/autoload.php';
 require '../src/Hello.php';
 
 $hello = new Hello();
 echo $hello->talk();
+
+
+//Exercice avec la vache Composer-3
+$joel = new Cow('Hello Farm!');
+$joel->say();
+$joel->setTongue('U');
+echo $joel;
